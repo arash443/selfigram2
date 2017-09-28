a44905311d5eab5960625dadbd91eaefec3623a1
arash443 <32114139+arash443@users.noreply.github.com>
2017-09-27 21:40:06 -0400

    Initial Commit

---
+++ selfigram2Tests/selfigram2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  selfigram2Tests.swift
+//  selfigram2Tests
+//
+//  Created by Arash  Daftarian on 2017-09-27.
+//  Copyright © 2017 Arash  Daftarian. All rights reserved.
+//
+
+import XCTest
+@testable import selfigram2
+
+class selfigram2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
